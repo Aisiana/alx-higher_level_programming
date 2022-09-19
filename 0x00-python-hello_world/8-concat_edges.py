dac9c533eff5a4e8cbd39ec0f16fa59de5d8b071
Aisiana <aisiana254@gmail.com>
2022-09-19 16:32:46 -0700

    Task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-        print(f"str{39:66} + {-22:-18} + {0:6}")
+        print(f"str{39:66} + {102:112} + {0:6}")
         print(str)
